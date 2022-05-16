9c6b308e15542121271d6065960d06ecb7c5828a
mostofa kamal <mostofakamal1990@gmail.com>
2022-05-16 09:13:52 +0600

    react query on RQsupperHeor.page.js

--- src/components/RQSupperHero.page.js
+++ src/components/RQSupperHero.page.js
@@ -1,9 +1,26 @@
+import axios from 'axios';
 import React from 'react';
+import {useQuery} from 'react-query'
+
+const supperHeroFetch = () =>{
+    return  axios.get("http://localhost:4000/supperHero");
+}
 
 const RQSupperHeroPage = () => {
+
+ const {isLoading, data, isError, error}=   useQuery('hero-unique-name', supperHeroFetch)
+
+//  console.log(isError)
+    if(isError){
+        return <h3 style={{color:"red"}}>{error.message}</h3>
+    }
+    if(isLoading){
+      return   <h2>Data is Loading Now ...</h2>
+    }
     return (
         <div>
-            <h2>Supper Hero Page</h2>
+            <h2>Supper Hero Page {data?.data.length}</h2>
+            {data?.data.map(hero => <h3 key={hero.id}>{hero.name}</h3>)}
         </div>
     );
 };
--- src/components/SupperHero.page.js
+++ src/components/SupperHero.page.js
@@ -5,16 +5,24 @@ const SupperHeroPage = () => {
 
     const [isLoading, setIsLoading] = useState(true);
     const [data, setData] = useState([]);
+    const [error , setError] = useState('');
     
     useEffect(()=>{
         axios.get("http://localhost:4000/supperHero").then(res => {
             setData(res.data);
             setIsLoading(false);
         
-        }
-            
-            );
+        }).catch(er => { console.log(er); setError(er.message); setIsLoading(false); });
     }, [])
+    if(error){
+      return  <h3>{error}</h3>
+    }
+    if(isLoading){
+      return  <h2>data is Loading....</h2>
+    }
+
+   
+   
     return (
         <div>
             <h2>Normal Supper hero Page {data.length}</h2>
